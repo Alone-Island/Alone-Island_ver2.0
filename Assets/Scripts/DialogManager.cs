ff4d359744affe5a52a23f0fd8f047929ddfb8fa
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-02 16:00:40 +0900

    Update DialogManager.cs

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class DialogManager : MonoBehaviour
 {
+
+    private TalkData talkData;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        talkData = GameObject.Find("TalkManger").GetComponent<TalkData>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //TalkData.
     }
 }
