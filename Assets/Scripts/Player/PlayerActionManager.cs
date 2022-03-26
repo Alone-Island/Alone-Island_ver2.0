d9db88fd0c8035a6c2cb1dd5493fe753afbd0b58
Jangmanbo <wkddpdnjs99@gmail.com>
2022-03-26 23:23:37 +0900

    add : detect animal
    플레이어가 동물 감지

--- Assets/Scripts/Player/PlayerActionManager.cs
+++ Assets/Scripts/Player/PlayerActionManager.cs
@@ -9,11 +9,14 @@ public class PlayerActionManager : MonoBehaviour
     private float range;    // J : 아이템을 습득 가능 범위
 
     private bool pickupActivated = false;  // J : 아이템 습득 가능 여부
+    private bool huntActivated = false;  // J : 사냥 가능 여부
 
     private RaycastHit2D hitInfo; // J : 충돌체의 정보
 
     [SerializeField]
-    private LayerMask layerMask;    // J : Item 레이어를 가지는 오브젝트만 습득해야 함
+    private LayerMask itemLayerMask;    // J : Item 레이어를 가지는 오브젝트만 습득해야 함
+    [SerializeField]
+    private LayerMask animalLayerMask;    // J : animal 레이어를 가지는 오브젝트만 습득해야 함
 
     // 필요한 컴포넌트
     [SerializeField]
@@ -34,6 +37,8 @@ public class PlayerActionManager : MonoBehaviour
     // J : 특정 행동 시도
     private void TryAction()
     {
+        CheckAnimal();
+        CanHunt();
         // J : E키를 눌렀을 때
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -42,11 +47,27 @@ public class PlayerActionManager : MonoBehaviour
         }
     }
 
+    // J : 플레이어 앞에 동물이 있는지 확인
+    private void CheckAnimal()
+    {
+        // J : 플레이어의 앞에 일정 범위 내에 있는 게임 오브젝트의 정보를 hitInfo에 저장
+        hitInfo = Physics2D.Raycast(transform.position, thePlayerMove.dirVec, range, animalLayerMask);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.transform.tag == "Animal")    // J : 오브젝트의 tag가 Animal
+            {
+                huntActivated = true;
+            }
+            else
+                huntActivated = false;
+        }
+    }
+
     // J : 플레이어가 주울 수 있는 아이템이 있는지 확인
     private void CheckItem()
     {
         // J : 플레이어의 앞에 일정 범위 내에 있는 게임 오브젝트의 정보를 hitInfo에 저장
-        hitInfo = Physics2D.Raycast(transform.position, thePlayerMove.dirVec, range, layerMask);
+        hitInfo = Physics2D.Raycast(transform.position, thePlayerMove.dirVec, range, itemLayerMask);
         if (hitInfo.collider != null)
         {
             if (hitInfo.transform.tag == "Item")    // J : 오브젝트의 tag가 Item
@@ -55,6 +76,17 @@ public class PlayerActionManager : MonoBehaviour
             }
             else
                 pickupActivated = false;
+        }
+    }
+
+    // J : 사냥 가능하면 사냥 장면으로 전환
+    private void CanHunt()
+    {
+        if (huntActivated)    // J : 사냥 가능 상태
+        {
+            Debug.Log(hitInfo.transform.gameObject.name + "을(를) 만났다!");
+            // 씬 전환
+            huntActivated = false;
         }
     }
 
