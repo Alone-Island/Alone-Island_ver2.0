6fc460e2ab905513a7ab0d901a8849047387d7e7
Jangmanbo <wkddpdnjs99@gmail.com>
2022-04-01 23:33:07 +0900

    change : Scene name (Hunting->EncounterAnimal)

--- Assets/Scripts/Hunting/HuntingManager.cs
+++ Assets/Scripts/Hunting/HuntingManager.cs
@@ -16,21 +16,5 @@ public class HuntingManager : MonoBehaviour
         
     }
 
-    // J : 사냥하자! 선택 시 호출
-    public void Hunt()
-    {
-        Debug.Log("사냥하자!");
-    }
-
-    // J : 길들이자! 선택 시 호출
-    public void Rear()
-    {
-        Debug.Log("길들이자!");
-    }
-
-    // J : 도망가자! 선택 시 호출
-    public void Run()
-    {
-        Debug.Log("도망가자!");
-    }
+    
 }
