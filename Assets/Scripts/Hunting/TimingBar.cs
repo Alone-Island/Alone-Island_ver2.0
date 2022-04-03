922e907856d5dfd7f5640cd25d7589419ca695ab
Jangmanbo <wkddpdnjs99@gmail.com>
2022-04-03 19:36:13 +0900

    add : judgement text
    적중 여부(SUCCESS / FAIL)를 나타내는 텍스트
    점점 커지게 -> fade out

--- Assets/Scripts/Hunting/TimingBar.cs
+++ Assets/Scripts/Hunting/TimingBar.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;    // J : TextMeshPro
 
 // https://namwhis.tistory.com/entry/Unity%EC%9C%A0%EB%8B%88%ED%8B%B0-%EC%A0%95%EB%A6%AC-04-%EC%A2%8C%EC%9A%B0%EC%9D%B4%EB%8F%99-%EB%AC%B4%ED%95%9C%EB%B0%98%EB%B3%B5-TimedeltaTime-Timetime-MathfSin
 public class TimingBar : MonoBehaviour
 {
-    Vector3 pos; //현재위치
-    float delta; // 좌우로 이동 가능한 x의 최대값
-    float[] successRange; // J : 적중 간격
+    private Vector3 pos; // J : 처음 화살표 위치
+    private float delta; // J : 좌우로 이동 가능한 x의 최대값
+    private float[] successRange; // J : 적중 간격
+    [SerializeField]
+    private float speed; // J : 화살표 이동 속도
 
+    public bool isSuccess; // J : 적중 여부
     public bool moveActivated = true;  // J : 화살표가 움직이는 중인지
 
-    [SerializeField]
-    float speed; // 이동속도
+    // J : 필요한 컴포넌트
     [SerializeField]
     private Transform Arrow;    // J : 화살표
     [SerializeField]
     private GameObject Bar;    // J : 바
+    [SerializeField]
+    private TextMeshProUGUI judgementText;  // J : SUCCESS / FAIL 텍스트
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +35,7 @@ public class TimingBar : MonoBehaviour
     void Update()
     {
         if (moveActivated)
-            Move();
+            Move(); // J : 화살표가 좌우로 이동
     }
 
     // J : 타이밍 적중 범위 결정
@@ -48,6 +53,7 @@ public class TimingBar : MonoBehaviour
         successBar.localScale = new Vector3(successBar.localScale.x / div, successBar.localScale.y, 0); // J : 적중 간격 설정
     }
 
+    // J : 화살표가 좌우로 이동
     private void Move()
     {
         Vector3 v = pos;
@@ -59,14 +65,67 @@ public class TimingBar : MonoBehaviour
     {
         moveActivated = false;  // J : 화살표 멈추기
 
-        // J : 적중 범위 내에서 멈추면
+        // J : 적중 범위 내에서 멈추면 성공
         if (successRange[0] <= Arrow.localPosition.x && Arrow.localPosition.x <= successRange[1])
         {
-            Debug.Log("Success");
+            Debug.Log("Hit Success");
+            isSuccess = true;
+
+            // J : SUCCESS 텍스트 표시
+            if (judgementText != null)
+                StartCoroutine("ShowSuccess");
         }
-        else
+        else // J : 실패
         {
-            Debug.Log("Fail");
+            Debug.Log("Hit Fail");
+            isSuccess = false;
+
+            // J : FAIL 텍스트 표시
+            if (judgementText != null) 
+                StartCoroutine("ShowFail");
+        }
+    }
+
+    // J : https://hyunity3d.tistory.com/410
+    // J : SUCCESS 텍스트 표시
+    IEnumerator ShowSuccess()
+    {
+        judgementText.text = "SUCCESS";
+        judgementText.color = new Vector4(0, 1, 0.5f, 1);
+
+        // J : 글자 크기 변경
+        for (int i = 0; i <= 50; i++)
+        {
+            judgementText.fontSize = i;
+            yield return new WaitForFixedUpdate();
+        }
+
+        // J : 글자 투명도 변경
+        for (float a = 1; a >= 0; a-=0.01f)
+        {
+            judgementText.color = new Vector4(0, 1, 0.5f, a);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    // J : FAIL 텍스트 표시
+    IEnumerator ShowFail()
+    {
+        judgementText.text = "FAIL";
+        judgementText.color = new Vector4(1, 0, 0, 1);
+
+        // J : 글자 크기 변경
+        for (int i = 0; i <= 50; i++)
+        {
+            judgementText.fontSize = i;
+            yield return new WaitForFixedUpdate();
+        }
+
+        // J : 글자 투명도 변경
+        for (float a = 1; a >= 0; a -= 0.01f)
+        {
+            judgementText.color = new Vector4(1, 0, 0, a);
+            yield return new WaitForFixedUpdate();
         }
     }
 }
