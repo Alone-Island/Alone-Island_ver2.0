7244339a0702111b3abe5ffd5e9d76c8b51d6409
Jangmanbo <wkddpdnjs99@gmail.com>
2022-04-17 03:46:28 +0900

    add : check animal's prefer food
    드롭한 식량 아이템이 동물이 좋아하는 음식인지 체크

--- Assets/Scripts/Hunting/RearManager.cs
+++ Assets/Scripts/Hunting/RearManager.cs
@@ -6,15 +6,16 @@ using TMPro;    // J : TextMeshPro
 
 public class RearManager : MonoBehaviour
 {
-    private GameObject prefab;  // J : 아이템 정보를 담는 프리팹 오브젝트
-    private Dictionary<Item, int> itemList;     // J : 식량 아이템 리스트
     [SerializeField]
     private Vector3 spawnPosition;  // J : 동물이 스폰될 위치
+    private Dictionary<Item, int> itemList;     // J : 식량 아이템 리스트
+    private Item dropItem;  // J : 플레이어가 드롭한 식량 아이템
 
     // 필요한 컴포넌트
     [SerializeField]
     private GameObject Content;  // J : 스크롤뷰의 Content 오브젝트
     private Inventory theInventory;
+    private GameObject prefab;  // J : 아이템 정보를 담는 프리팹 오브젝트
 
     // Start is called before the first frame update
     void Start()
@@ -54,16 +55,32 @@ public class RearManager : MonoBehaviour
     {
         if (theInventory.ConsumeItem(obj.GetComponent<ItemInfo>().item))    // J : 아이템이 1개 이상 있으면
         {
-            itemList[obj.GetComponent<ItemInfo>().item] -= 1;   // J : 아이템 리스트에서 개수 업데이트
+            dropItem = obj.GetComponent<ItemInfo>().item;
+            itemList[dropItem] -= 1;   // J : 아이템 리스트에서 개수 업데이트
             // J : 화면, ItemInfo 에서 갱신
-            obj.transform.Find("ItemCount").GetComponent<TextMeshProUGUI>().text = itemList[obj.GetComponent<ItemInfo>().item].ToString();
-            obj.GetComponent<ItemInfo>().count = itemList[obj.GetComponent<ItemInfo>().item];
+            obj.transform.Find("ItemCount").GetComponent<TextMeshProUGUI>().text = itemList[dropItem].ToString();
+            obj.GetComponent<ItemInfo>().count = itemList[dropItem];
 
             // J : 음식 떨어트리기
             Vector3 spawnPosition = GameObject.Find("Player").transform.position;
             spawnPosition.x += GameObject.Find("Player").transform.localScale.x;
-            GameObject foodObject = Instantiate(obj.GetComponent<ItemInfo>().item.itemPrefab, spawnPosition, Quaternion.identity);    // J : 음식 오브젝트 스폰
+            GameObject foodObject = Instantiate(dropItem.itemPrefab, spawnPosition, Quaternion.identity);    // J : 음식 오브젝트 스폰
             foodObject.GetComponent<Rigidbody2D>().gravityScale = 1;  // J : 횡스크롤이므로 중력 적용
+
+            checkPreferFood();
+        }
+    }
+
+    private void checkPreferFood()
+    {
+        List<FoodItem.FoodType> preferFoods = GameData.encounterAnimal.preferFoods; // J : 동물이 좋아하는 음식 유형
+        if (preferFoods.Contains(((FoodItem)dropItem).foodType))    // J : 드롭한 음식이 동물이 좋아하는 유형이면
+        {
+            Debug.Log("길들이기 성공 확률 증가");
+        }
+        else
+        {
+            Debug.Log("길들이기 실패");
         }
     }
 }
