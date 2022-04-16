f6c66c08901bac2c6bb5ecb4d73fdd994adb434d
Jangmanbo <wkddpdnjs99@gmail.com>
2022-04-17 03:23:38 +0900

    add : TestJ_hunt->TestK scene button

---
+++ Assets/Scripts/TempScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TempScript : MonoBehaviour
+{
+    public void CraftScene()
+    {
+        SceneManager.LoadScene("TestK");
+    }
+}
