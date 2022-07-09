fe5eb08dcb50455f5ce4b58a859ce8721ce101ac
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-07-10 04:33:01 +0900

    Update craft
    공예 성공

--- Assets/Scripts/Craft/CraftManager.cs
+++ Assets/Scripts/Craft/CraftManager.cs
@@ -1,50 +1,124 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftManager : MonoBehaviour
 {
     // 필요한 컴포넌트
     [SerializeField]
-    private ItemData itemData;
-    private Inventory theInventory;
+    public ItemData ItemData;
+    private Inventory Inventory;
+
+    public GameObject CraftContent;
+    public GameObject CraftComplete;
+    public List<ItemData.ItemDictionary> itemData;
 
     // Start is called before the first frame update
     void Start()
     {
-        theInventory = FindObjectOfType<Inventory>();
-        itemData = FindObjectOfType<ItemData>();
+        Inventory = FindObjectOfType<Inventory>();
+        ItemData = FindObjectOfType<ItemData>();
+        //CraftContent = GameObject.Find("Content");
 
-        
+        itemData = new List<ItemData.ItemDictionary>();
+        // J : 공예 데이터 추가 (예시)
+        itemData.Add(new ItemData.ItemDictionary(
+            Resources.Load<Item>("Item/Equipment/Shovel"),
+            new List<ItemData.Items> {
+                new ItemData.Items(Resources.Load<Item>("Item/Food/Apple"), 1),
+                new ItemData.Items(Resources.Load<Item>("Item/Food/Banana"), 2)
+            }));
+        itemData.Add(new ItemData.ItemDictionary(
+            Resources.Load<Item>("Item/Food/Pork"),
+            new List<ItemData.Items> {
+                new ItemData.Items(Resources.Load<Item>("Item/Food/Carrot"), 2)
+            }));
+        itemData.Add(new ItemData.ItemDictionary(
+            Resources.Load<Item>("Item/Food/Pork"),
+            new List<ItemData.Items> {
+                new ItemData.Items(Resources.Load<Item>("Item/Food/Carrot"), 2)
+            }));
+
+        for (int i = 0; i < itemData.Count; i++)
+        {
+            ItemData.ItemDictionary item = itemData[i];
+            GameObject craftItemList = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/UI/Craft/CraftItemList") as GameObject) as GameObject;
+            craftItemList.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = item.item.itemImage;
+            craftItemList.transform.SetParent(CraftContent.transform, false);
+
+            for (int j = 0; j < item.materials.Count; j++)
+            {
+                GameObject material = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/UI/Craft/Material") as GameObject) as GameObject;
+                material.transform.SetParent(craftItemList.transform.GetChild(2), false);
+
+                material.transform.GetChild(0).GetComponent<Image>().sprite = item.materials[j].item.itemImage;
+                material.transform.GetChild(2).GetComponent<Text>().text = item.materials[j].num.ToString();
+
+                if (j < item.materials.Count - 1)
+                {
+                    GameObject addMaterial = PrefabUtility.InstantiatePrefab(Resources.Load("Prefabs/UI/Craft/AddMaterial") as GameObject) as GameObject;
+                    addMaterial.transform.SetParent(craftItemList.transform.GetChild(2), false);
+                }
+            }
+
+            if (CheckCanMakeItem(item.materials))
+            {
+                craftItemList.GetComponent<Button>().enabled = false;
+            } else
+            {
+                craftItemList.GetComponent<Button>().enabled = true;
+            }
+
+            craftItemList.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                if (MakeNewItem(item))
+                {
+                    CraftComplete.transform.GetChild(1).GetChild(1).GetComponent<Image>().sprite = item.item.itemImage;
+                    CraftComplete.SetActive(true);
+                }
+            });
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (ItemData != null)
+        {
+            
+        }
     }
 
     // K : 인벤토리에 필요한 재료가 모두 있으면 재료 리스트 리턴, 아니면 null 리턴
-    private List<ItemData.Items> CheckMakeItem(Item _item) {
-        List<ItemData.Items> materials = itemData.GetItemMaterialsData(_item);  // J : 아이템을 만드는데 필요한 재료 리스트 받아오기
+    private Boolean CheckCanMakeItem(List<ItemData.Items> materials) {
+        //List<ItemData.Items> materials = ItemData.GetItemMaterialsData(_item);  // J : 아이템을 만드는데 필요한 재료 리스트 받아오기
 
-        foreach (ItemData.Items material in materials)  // J : 재료 + 재료의 개수
-            if (material.num < theInventory.GetItemCount(material.item))    // J : 인벤토리에 재료의 개수가 필요 개수보다 적다면 만들기 불가능
-                return null;
+        foreach (ItemData.Items material in materials)
+        { // J : 재료 + 재료의 개수
+            if (material.num > Inventory.GetItemCount(material.item))    // J : 인벤토리에 재료의 개수가 필요 개수보다 적다면 만들기 불가능
+                return false;
+        }
 
-        return materials;   // J : 만들기 가능하면 재료 리스트 리턴
+        return true;   // J : 만들기 가능하면 재료 리스트 리턴
     }
 
     // K : 아이템 만들기 성공 여부 리턴
-    public bool MakeNewItem(Item _item) {
-        List<ItemData.Items> materials = CheckMakeItem(_item);
-
-        if (materials == null)  // J : 인벤토리에 존재하는 아이템들로는 _item 만들기 불가능
+    public bool MakeNewItem(ItemData.ItemDictionary _item) {
+        if (!CheckCanMakeItem(_item.materials))  // J : 인벤토리에 존재하는 아이템들로는 _item 만들기 불가능
         {
-            Debug.Log(_item.itemName + " 획득 실패");
+            Debug.Log(_item.item.name + " 획득 실패");
             return false;
         }
 
         // J : 인벤토리의 재료 아이템 소비
-        foreach (ItemData.Items material in materials)
-            theInventory.ConsumeItem(material.item, material.num);
-        theInventory.AcquireItem(_item);    // K : 인벤토리의 슬롯에 아이템 추가
-        Debug.Log(_item.itemName + " 획득 성공");
+        foreach (ItemData.Items material in _item.materials)
+            Inventory.ConsumeItem(material.item, material.num);
+        Inventory.AcquireItem(_item.item);    // K : 인벤토리의 슬롯에 아이템 추가
+        Debug.Log(_item.item.name + " 획득 성공");
 
         return true;
     }
