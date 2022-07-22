6a947bd44c54eb187169c289849681dbd226f426
Jangmanbo <wkddpdnjs99@gmail.com>
2022-07-23 00:51:21 +0900

    add : animal move to food
    플레이어가 음식을 떨어트리면 동물이 음식을 향해 이동

--- Assets/Scripts/Hunting/RearManager.cs
+++ Assets/Scripts/Hunting/RearManager.cs
@@ -7,17 +7,20 @@ using TMPro;    // J : TextMeshPro
 public class RearManager : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 spawnPosition;  // J : 동물이 스폰될 위치
+    private Vector3 spawnPosition;              // J : 동물이 스폰될 위치
     private Dictionary<Item, int> itemList;     // J : 식량 아이템 리스트
-    private Item dropItem;  // J : 플레이어가 드롭한 식량 아이템
-    private GameObject animalObject;    // J : 스폰한 동물 오브젝트
-    private bool isDropFood;
+    private Item dropItem;                      // J : 플레이어가 드롭한 식량 아이템
+    private GameObject animalObject;            // J : 스폰한 동물 오브젝트
+    private GameObject foodObject;                      // J : 스폰한 음식 오브젝트
+    private bool isDropFood;                    // J : 음식을 떨어트렸는지 여부
 
     // 필요한 컴포넌트
     [SerializeField]
     private GameObject Content;  // J : 스크롤뷰의 Content 오브젝트
     private Inventory theInventory;
     private GameObject prefab;  // J : 아이템 정보를 담는 프리팹 오브젝트
+    [SerializeField]
+    private LayerMask itemLayerMask;    // J : 동물이 Item 레이어를 가지는 오브젝트(음식) 감지
 
     // Start is called before the first frame update
     void Start()
@@ -66,24 +69,71 @@ public class RearManager : MonoBehaviour
             // J : 음식 떨어트리기
             Vector3 spawnPosition = GameObject.Find("Player").transform.position;
             spawnPosition.x += GameObject.Find("Player").transform.localScale.x;
-            GameObject foodObject = Instantiate(dropItem.itemPrefab, spawnPosition, Quaternion.identity);    // J : 음식 오브젝트 스폰
+            foodObject = Instantiate(dropItem.itemPrefab, spawnPosition, Quaternion.identity);    // J : 음식 오브젝트 스폰
             foodObject.GetComponent<Rigidbody2D>().gravityScale = 1;  // J : 횡스크롤이므로 중력 적용
             isDropFood = true;  // J : 음식을 하나만 떨어트리도록
 
-            checkPreferFood();
+            StartCoroutine("AnimalGoToFood");   // J : 동물이 음식을 향해 이동
         }
     }
 
-    private void checkPreferFood()
+    private void RearResult()
     {
-        List<FoodItem.FoodType> preferFoods = DataController.Instance.gameData.encounterAnimal.preferFoods; // J : 동물이 좋아하는 음식 유형
-        if (preferFoods.Contains(((FoodItem)dropItem).foodType))    // J : 드롭한 음식이 동물이 좋아하는 유형이면
+        if (CheckPreferFood())  // J : 플레이어가 떨어트린 음식이 동물이 선호하는 음식인 경우
         {
-            Debug.Log("길들이기 성공 확률 증가");
+            Debug.Log("길들이기 성공");
         }
         else
         {
             Debug.Log("길들이기 실패");
+        }
+    }
+
+    // J : 동물이 음식을 향해 이동
+    IEnumerator AnimalGoToFood()
+    {
+        Vector3 destination = foodObject.transform.position;
+        while (true)
+        {
+            Vector3 dir = new Vector3(destination.x - animalObject.transform.position.x, 0, 0);
+            animalObject.transform.position += dir * 1 * Time.deltaTime;
+
+            // J : 트리거
+            if (AnimalCheckFood(dir, 1f))
+                break;
+
+            yield return null;
+        }
+        RearResult();
+    }
+
+    // J : 동물 앞에 아이템이 있는지 확인
+    private bool AnimalCheckFood(Vector3 dir, float range)
+    {
+        // J : 동물 앞에 일정 범위 내에 있는 게임 오브젝트의 정보를 hitInfo에 저장
+        Debug.DrawRay(animalObject.transform.position, dir * range, Color.green);   // J : 아이템 습득 가능 범위 표시
+        RaycastHit2D hitInfo = Physics2D.Raycast(animalObject.transform.position, dir, range, itemLayerMask);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.transform.tag == "Item")    // J : 오브젝트의 tag가 Item
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+    // J : 플레이어가 떨어트린 음식이 동물의 선호 음식인지 확인
+    private bool CheckPreferFood()
+    {
+        List<FoodItem.FoodType> preferFoods = DataController.Instance.gameData.encounterAnimal.preferFoods; // J : 동물이 좋아하는 음식 유형
+        if (preferFoods.Contains(((FoodItem)dropItem).foodType))    // J : 드롭한 음식이 동물이 좋아하는 유형이면
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
